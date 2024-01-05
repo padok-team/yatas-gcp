079c88f7d67bc4d39d2dce4a8e2787c2314a63dc
Luca Corrieri <lucac@padok.fr>
2024-01-05 15:42:41 +0100

    wip: getter for service account keys

--- gcp/iam/getter.go
+++ gcp/iam/getter.go
@@ -10,6 +10,7 @@ import (
 	resourcemanager "cloud.google.com/go/resourcemanager/apiv3"
 	"github.com/padok-team/yatas-gcp/internal"
 	"github.com/padok-team/yatas-gcp/logger"
+	"google.golang.org/api/iterator"
 )
 
 // Return the permissions for the given role
@@ -69,4 +70,57 @@ func GetPermissionsByServiceAccounts(account internal.GCPAccount) map[string][]s
 	}
 
 	return permissionsBySA
+}
+
+// Get the keys associated to a service account
+func getKeysOfServiceAccount(sa *adminpb.ServiceAccount, ctx context.Context, c *admin.IamClient) []*adminpb.ServiceAccountKey {
+	req := &adminpb.ListServiceAccountKeysRequest{
+		Name: sa.GetName(),
+	}
+	resp, err := c.ListServiceAccountKeys(ctx, req)
+	if err != nil {
+		logger.Logger.Error("Failed to list service accounts keys", "serviceAccount", sa.GetName(), "error", err)
+		return nil
+	}
+
+	return resp.GetKeys()
+}
+
+// Produce a list of service account keys existing in the GCP project
+func GetServiceAccountKeys(account internal.GCPAccount) []*adminpb.ServiceAccountKey {
+	ctx := context.Background()
+
+	c, err := admin.NewIamClient(ctx)
+	if err != nil {
+		logger.Logger.Error("Failed to create IAM client", "error", err)
+		return nil
+	}
+	defer c.Close()
+
+	req := &adminpb.ListServiceAccountsRequest{
+		Name: "projects/" + account.Project,
+	}
+	var saList []*adminpb.ServiceAccount
+	it := c.ListServiceAccounts(ctx, req)
+	for {
+		resp, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			logger.Logger.Error("Failed to list service accounts for project", "project", account.Project, "error", err)
+			return nil // TODO
+		}
+
+		saList = append(saList, resp)
+	}
+
+	// Get all service accounts keys and merge them into a single list
+	var keys []*adminpb.ServiceAccountKey
+	for _, sa := range saList {
+		saKeys := getKeysOfServiceAccount(sa, ctx, c)
+		keys = append(keys, saKeys...)
+	}
+
+	return keys
 }
