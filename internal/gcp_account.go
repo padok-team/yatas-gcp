0d146f969c572df80b9530f6a5bad742c63f8483
riptz5 <leandroalvarez@gmail.com>
2026-08-22 17:21:09 -0400

    feat: add support for Organization and Folder configuration (#4)

--- internal/gcp_account.go
+++ internal/gcp_account.go
@@ -3,4 +3,6 @@ package internal
 type GCPAccount struct {
 	Project        string   `yaml:"project"`        // Name of the account in the reports
 	ComputeRegions []string `yaml:"computeRegions"` // List of regions to make Compute API calls
+	Organization   string   `yaml:"organization"`   // Organization ID to scan projects
+	Folder         string   `yaml:"folder"`         // Folder ID to scan projects
 }
